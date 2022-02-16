5daef8caa1dc9c04993e938619c87693d429e993
Alan <me@alan.me>
2022-02-16 12:13:19 +0100

    correction P6

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,31 +1,80 @@
-   
-function photographersFactory(data) {
-    const { name, city, tagline, country, price, portrait } = data;//remplacer les
+let photographersData;
 
-    const picture = `assets/photographers/${portrait}`;
+//tableau photographers contient les données des photographes
+async function getPhotographerData() {
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );//créer un élement article
-        const photographerLinkPage = document.createElement('a');
-        photographerLinkPage.setAttribute("href", "photographer.html?id=");
-        const img = document.createElement( 'img' );//Créer un élement img
-        img.setAttribute("src", picture)//Affecter une image et une src
-        photographerLinkPage.appendChild(img);
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        const h3 = document.createElement('h3');
-        h3.textContent = city + ', ' + country;
-        const p = document.createElement('p');
-        p.textContent = tagline;
-        const h5 = document.createElement('h5');
-        h5.textContent = price + '€/jour';
-        //article.appendChild(img);//Ajouter l'élement img
-        article.appendChild(photographerLinkPage);
-        article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(p);
-        article.appendChild(h5);
-        return (article);
-    }
-    return { getUserCardDOM }//retourner un objet
+    //récupérer l'id dans l'url
+
+    const id = 82;
+
+    // récupérer les données de tous les photographes
+
+    // filtrer les données reçues pour ne garder que celles avec l'id du photographe souhaité
+    photographersData.photographer = photographersData.photographers.filter((photographer) => photographer.id == id)[0];
+
+    photographersData.media = ????????;
+
+    return photographersData;
+
+    //Construction de l'élement photograph-header <<== a mettre dans la factory
+    const photographHeader = document.getElementById('photograph-header');
+    const description = document.createElement('article');
+    photographHeader.appendChild(description);
+    const h2 = document.createElement('h2');
+    h2.textContent = photographers[0].name;
+    description.appendChild(h2);
+    const h3 = document.createElement('h3');
+    h3.textContent = photographers[0].city +  ", " + photographers[0].country;
+    description.appendChild(h3);
+    const p = document.createElement('p');
+    p.textContent = photographers[0].tagline;
+    description.appendChild(p);
+    const button = document.querySelector('.contact_button');
+    photographHeader.appendChild(button);
+    const picture = document.createElement('img');
+    picture.setAttribute("src", "assets/photographers/MimiKeel.jpg");
+    photographHeader.appendChild(picture);
 }
+
+function init(){
+    const { photographer, media } = await getPhotographerData();
+}
+
+
+//Création de la partie Trier par <<== a mettre dans la factory
+const mediaSection = document.createElement('section');
+const main = document.querySelector('main');
+main.appendChild(mediaSection);
+const h4 = document.createElement('h4');
+h4.textContent = "Trier par";
+mediaSection.appendChild(h4);
+const ul = document.createElement('ul');
+mediaSection.appendChild(ul);
+const angleUp = document.createElement('i');
+angleUp.setAttribute('class', 'fa-solid fa-angle-up');
+const popular = document.createElement('li');
+popular.setAttribute('class', 'popular');
+const popularAngleUp = document.createElement('div');
+popularAngleUp.setAttribute('class', 'popular-angleUp');
+ul.appendChild(popularAngleUp);
+popularAngleUp.appendChild(angleUp);
+popularAngleUp.appendChild(popular);
+const date = document.createElement('li');
+date.setAttribute('class', 'date');
+const title = document.createElement('li');
+title.setAttribute('class', 'title');
+popular.textContent = "Popularité";
+date.textContent = "Date";
+title.textContent = "Titre";
+ul.appendChild(date);
+ul.appendChild(title);
+
+//Cette fonction affiche le bloc Trier par
+function displayOrderBy() {
+    title.style.display = 'block';
+    date.style.display = 'block'; 
+}
+
+angleUp.addEventListener('click', displayOrderBy);
+
+init();
