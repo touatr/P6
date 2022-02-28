4059870d4dc47d8379295306bca2672917f23895
touatr <touati.ram@gmail.com>
2022-02-28 20:50:35 +0100

    Affichage de chaque photographe

--- scripts/factories/onePhotographerFactory.js
+++ scripts/factories/onePhotographerFactory.js
@@ -1,27 +1,27 @@
 //Factory qui construit la page d'un photographe en fonction de l'id envoyé dans l'url
 function onePhotographerFactory(data) {
-    const { id, name, city, tagline, country, portrait } = data;
+    const { name, city, tagline, country, portrait } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getPhotographerPage() {
         //Construction de l'élément photograph-header
         const description = document.createElement('article');
-        const photographHeader = document.createElement('div');
-        photographHeader.appendChild(description);
+        const text = document.createElement('aside');
+        description.appendChild(text);
         const h2 = document.createElement('h2');
         h2.textContent = name;
-        description.appendChild(h2);
+        text.appendChild(h2);
         const h3 = document.createElement('h3');
         h3.textContent = city +  ", " + country;
-        description.appendChild(h3);
+        text.appendChild(h3);
         const p = document.createElement('p');
         p.textContent = tagline;
-        description.appendChild(p);
-        // const button = document.querySelector('.contact_button');
-        // photographHeader.appendChild(button);
+        text.appendChild(p);
+        const button = document.querySelector('.contact_button');
+        description.appendChild(button);
         const img = document.createElement('img');
         img.setAttribute("src", picture);
-        photographHeader.appendChild(img);
+        description.appendChild(img);
         return (description);
     }
 
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,7 +4,14 @@ async function getPhotographers() {
 	let photographersData = await photographers.json();
 
     // étape 1 : récupérer l'id en paramètre avec URLSearchParams
-    // const id = ?????
+    //Récupérer la chaine de requête dans l'url
+    const queryString = window.location.search;
+    console.log(queryString);
+    //Récupérer l'id depuis l'url
+    const urlSearchParams = new URLSearchParams(queryString);
+    console.log(urlSearchParams);
+    const id = urlSearchParams.get("id");
+    console.log(id);
 
     // étape 2 : filtrer les photographes pour ne récupérer que celui avec l'id qu'on a récupéré
     let photograph = photographersData.photographers.filter((photographer) => photographer.id == id );
@@ -13,11 +20,12 @@ async function getPhotographers() {
 	return photograph[0];
 }
 
-async function displayDataPhotograh(photographer) {
+function displayDataPhotograh(photographer) {
     const photographHeader = document.getElementById('photograph-header');
 
-    const photographerModel = getPhotographerPage(photographer);
-    
+    const photographerModel = onePhotographerFactory(photographer);
+    const userCardDOM = photographerModel.getPhotographerPage();
+    photographHeader.appendChild(userCardDOM);
 };
 
 async function init() {
@@ -28,3 +36,39 @@ async function init() {
 };
 
 init();
+
+//Création de la partie Trier par
+const mediaSection = document.createElement('section');
+const main = document.querySelector('main');
+main.appendChild(mediaSection);
+const h4 = document.createElement('h4');
+h4.textContent = "Trier par";
+mediaSection.appendChild(h4);
+const ul = document.createElement('ul');
+mediaSection.appendChild(ul);
+const angleUp = document.createElement('i');
+angleUp.setAttribute('class', 'fa-solid fa-angle-up');
+const popular = document.createElement('li');
+popular.setAttribute('class', 'popular');
+const popularAngleUp = document.createElement('div');
+popularAngleUp.setAttribute('class', 'popular-angleUp');
+ul.appendChild(popularAngleUp);
+popularAngleUp.appendChild(angleUp);
+popularAngleUp.appendChild(popular);
+const date = document.createElement('li');
+date.setAttribute('class', 'date');
+const title = document.createElement('li');
+title.setAttribute('class', 'title');
+popular.textContent = "Popularité";
+date.textContent = "Date";
+title.textContent = "Titre";
+ul.appendChild(date);
+ul.appendChild(title);
+
+//Cette fonction affiche le bloc Trier par
+function displayOrderBy() {
+    title.style.display = 'block';
+    date.style.display = 'block'; 
+}
+
+angleUp.addEventListener('click', displayOrderBy);
